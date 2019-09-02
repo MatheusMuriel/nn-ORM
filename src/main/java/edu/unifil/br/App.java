6b94db3889d89e8665f974d2a8df2f5ab060d0b5
Matheus Muriel <matheus.muriel@outlook.com>
2019-09-02 19:41:29 -0300

    Commit inicial

---
+++ src/main/java/edu/unifil/br/App.java
@@ -0,0 +1,12 @@
+package edu.unifil.br;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
